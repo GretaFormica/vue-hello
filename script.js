c24084b34b71866fbe84b737f68e495d06f727f0
GretaFormica <greta.formica@gmail.com>
2023-02-10 09:51:05 +0100

    correzione

--- script.js
+++ script.js
@@ -5,22 +5,10 @@ const app = Vue.createApp({
     data() { //metodo data
 
         return {
-            message: 'hello!'
+            message: 'hello!',
+            image: 'https://static.macupdate.com/products/54025/l/visual-studio-code-logo.png?v=1671090395'
         }
     }
 });
 
-app.mount("#h1");
-
-//bonus
-const image = Vue.createApp({
-
-    data() { //metodo data
-
-        return {
-            message: '<img src="https://static.macupdate.com/products/54025/l/visual-studio-code-logo.png?v=1671090395" alt="">'
-        }
-    }
-});
-
-image.mount("#img");
+app.mount("#root");
